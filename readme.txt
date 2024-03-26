c272a1c45c4d73770da348dc0e2bea75740c847b
Russell Snyder <russell@rustybass.comr>
2024-03-26 11:54:38 +0100

    add readme

---
+++ readme.txt
@@ -0,0 +1,11 @@
+
+
+Menu items
+
+http://127.0.0.1:8000/api/menu-items/
+
+
+Bookings
+
+http://127.0.0.1:8000/api/bookings/
+
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.index, name='home'), 
     path('menu-items/', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('bookings/', views.BookingViewSet.as_view({
+        'get': 'list',
+        'post': 'create',
+        'put': 'update',
+        })),
 ]
